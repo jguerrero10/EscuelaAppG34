eca59613f4a732209e7489c14d77c8b307df42d1
Joel Guerrero <joel.guerrero@canalclima.com>
2022-08-30 08:50:54 -0500

    Edición de plantillas

--- escuelaapp/urls.py
+++ escuelaapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
-    path('listar/profesor/', list_profesor),
-    path('crear/profesor/', crear_profesor),
-    path('actualizar/profesor/<int:id>', actualizar_profesor),
-    path('eliminar/profesor/<int:id>', eliminar_profesor)
+    path('', home, name='home'),
+    path('listar/profesor/', list_profesor, name='lista-profesor'),
+    path('crear/profesor/', crear_profesor, name='crear-profesor'),
+    path('actualizar/profesor/<int:id>', actualizar_profesor, name='actualizar-profesor'),
+    path('eliminar/profesor/<int:id>', eliminar_profesor, name='eliminar-nombre')
 ]

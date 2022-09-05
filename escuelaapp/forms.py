1270bfffbcc07957db0fca4387f16d063b072b91
Joel Guerrero <joel.guerrero@canalclima.com>
2022-09-05 08:57:11 -0500

    se actualiza formulario profesor

--- escuelaapp/forms.py
+++ escuelaapp/forms.py
@@ -3,8 +3,17 @@ from django import forms
 from .models import *
 
 class ProfesorForm(forms.ModelForm):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(max_length=40, widget=(forms.PasswordInput()))
+    username = forms.CharField(max_length=20, label="Usuario", widget=forms.TextInput(
+        attrs={
+            'class': "form-control",
+            'placeholder': "Nombre de usuario"
+        }
+    ))
+    password = forms.CharField(max_length=40, widget=(forms.PasswordInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
     email = forms.EmailField()
     nombres = forms.CharField(max_length=120)
     apellidos = forms.CharField(max_length=120)

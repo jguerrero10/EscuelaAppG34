6252c47849d9b79fae957231898a93e8ac7ba555
Joel Guerrero <joel.guerrero@canalclima.com>
2022-09-26 08:12:49 -0500

    Se agrega configuraciones heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn escuela.wsgi --log-file -
--- escuela/settings.py
+++ escuela/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from email import message
 from pathlib import Path
 from django.contrib.messages import constants as mensaje
 import os
